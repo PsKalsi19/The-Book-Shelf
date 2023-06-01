e0ed18f25eced5cd27f03e5d1d8a6c814b771c0c
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2023-06-01 12:07:22 +0530

    [neog-25] feat: Added loader support for products overview and list.

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,15 +1,27 @@
-
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BooksContext } from "../../contexts/BooksProvider";
 
-import ProductCard from "../../components/products/ProductCard"
+import ProductCard from "../../components/products/ProductCard";
+import Loader from "../../components/loader/Loader";
 const Products = () => {
-  const {booksState:{booksData}}=useContext(BooksContext);
+  const [showLoader, setShowLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowLoader(false);
+    }, 2000);
+  }, []);
+  const {
+    booksState: { booksData },
+  } = useContext(BooksContext);
+
+  if(showLoader) return <Loader/>
   return (
     <div className="grid grid-cols-2 gap-2 lg:grid-cols-4">
-      {
-        booksData && booksData.length>0 && booksData.map(product=> <ProductCard key={product._id} product={product} />)
-      }
+      {booksData &&
+        booksData.length > 0 &&
+        booksData.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
     </div>
   );
 };
