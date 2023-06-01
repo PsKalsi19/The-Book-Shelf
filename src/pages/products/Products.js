3e3dfc2aa24c5ee9a9969ebb57a9cca865d218d7
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2023-06-01 16:58:11 +0530

    [neog-26] feat: Added search support.

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -5,21 +5,32 @@ import ProductCard from "../../components/products/ProductCard";
 import Loader from "../../components/loader/Loader";
 const Products = () => {
   const [showLoader, setShowLoader] = useState(true);
+  const { booksState, searchProductsHandler } = useContext(BooksContext);
+
   useEffect(() => {
-    setTimeout(() => {
+    const loader = setTimeout(() => {
       setShowLoader(false);
     }, 2000);
+    return () => clearTimeout(loader);
   }, []);
-  const {
-    booksState: { booksData },
-  } = useContext(BooksContext);
 
-  if(showLoader) return <Loader/>
+  // useEffect(()=>{
+  //   const searchTimeout=setTimeout(() => {
+      
+  //     searchProductsHandler()
+  //   }, 10000);
+  //   return ()=>clearTimeout(searchTimeout)
+  // },[searchTerm])
+
+
+
+  const { booksData } = booksState;
+  if (showLoader) return <Loader />;
   return (
     <div className="grid grid-cols-2 gap-2 lg:grid-cols-4">
       {booksData &&
         booksData.length > 0 &&
-        booksData.map((product) => (
+        searchProductsHandler().map((product) => (
           <ProductCard key={product._id} product={product} />
         ))}
     </div>
