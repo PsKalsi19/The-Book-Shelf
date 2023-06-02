2d4049e0a86d2c30c303f50eab16269f68f47ae5
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2023-06-03 02:19:48 +0530

    [neog-30] feat: done with category select from homepage.

--- src/components/products/filters/Range.js
+++ src/components/products/filters/Range.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Loader from '../../loader/Loader';
 
 const Range = ({value,dispatch,heading,max,step,dispatchType}) => {
+    const [showLoader,setShowLoader]=useState(false)
     const handleSliderChange=(e)=>{
         dispatch({type:dispatchType,payload:Number(e.target.value)})
     }
+ 
     return (
         <fieldset className='pb-4 border-b border-gray-500'>
             <legend className="text-base text-gray-100">{heading}</legend>
@@ -11,6 +14,8 @@ const Range = ({value,dispatch,heading,max,step,dispatchType}) => {
             type="range" 
             min="0" 
             max={max}
+            onMouseDown={()=>setShowLoader(true)}
+            onMouseUp={()=>setShowLoader(false)}
             onChange={handleSliderChange}
             value={value} 
             step={step} 
